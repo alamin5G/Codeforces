4ff2ea78e955414433637fb4c99fcc513d95deda
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-25 01:13:51 +0600

    see the problem - https://prnt.sc/4VcSlsJkUPcG

---
+++ codeforces231A.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file codeforces231A.cpp
+ * see the problem - https://prnt.sc/4VcSlsJkUPcG
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-08-25
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    int count=0;
+    cin >> t;
+    while(t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a+b+c >=2){
+            count++;
+        }
+
+    }
+    cout << count << endl;
+    return 0;
+}
