b22ca5b172e812fae8a7e2017e050f71edd14603
Md. Alamin <alaminrakib4864@gmail.com>
2022-08-27 01:07:27 +0600

    see the problem - https://codeforces.com/problemset/problem/281/A

---
+++ codeforces281A.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file codeforces281A.cpp
+ * see the problem - https://codeforces.com/problemset/problem/281/A
+ * @author Md. Alamin (alamin5g@yahoo.com)
+ * I would love be a software engineer at Google. That is why anybody can uses this code without any condition, if you face any difficulty, then try to email me.
+ * @version 0.1
+ * @date 2022-08-27
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    s[0] = toupper(s[0]);
+    cout << s << endl;
+
+    return 0;
+}
